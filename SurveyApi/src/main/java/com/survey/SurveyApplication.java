f038c424bbf0901c3647940344c38a2c63290fb8
ezdyveg <Corban.Stevens@McKesson.com>
2020-07-17 14:05:25 -0700

    Application added so services can be started

---
+++ SurveyApi/src/main/java/com/survey/SurveyApplication.java
@@ -0,0 +1,25 @@
+package com.survey;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("STARTED");
+
+        };
+    }
+
+}
